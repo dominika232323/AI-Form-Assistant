8ea2d68080ebf1c9db9d8c0e4f41127f5421721e
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-05-07 18:29:53 +0200

    Move display_form function to form.py

--- app/main.py
+++ app/main.py
@@ -1,38 +1,6 @@
+from form import display_form
 from assistant import get_response, get_gemini_api_key
 import streamlit as st
-
-
-def display_form(form_data: dict):
-    with st.form("contact_form"):
-        st.text_input("Firstname",
-                      value=form_data.get("firstname", ""),
-                      max_chars=20,
-                      key="firstname",
-                      disabled=True)
-
-        st.text_input("Lastname",
-                      value=form_data.get("lastname", ""),
-                      max_chars=20,
-                      key="lastname",
-                      disabled=True)
-
-        st.text_input("Email",
-                      value=form_data.get("email", ""),
-                      key="email",
-                      disabled=True)
-
-        st.text_area("Reason of contact",
-                     value=form_data.get("reason", ""),
-                     max_chars=100,
-                     key="reason",
-                     disabled=True)
-
-        st.slider("Urgency",
-                  min_value=1,
-                  max_value=10,
-                  value=form_data.get("urgency", 5),
-                  key="urgency",
-                  disabled=True)
 
 
 def main():
